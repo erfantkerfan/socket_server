9e75af1dfb22a199d9fcdb146efd84aec7cf7cc8
shayan <yusefshayan@gmail.com>
2021-11-20 18:43:28 +0330

    Add clustering

--- socket-server.ts
+++ socket-server.ts
@@ -2,60 +2,81 @@ import express from 'express';
 import {createServer} from "http";
 import {Server} from "socket.io";
 import yargs from "yargs";
-import cluster from "cluster";
-import os from "os";
+import cluster, {Worker} from "cluster";
 
 yargs(process.argv.slice(2))
     .command('*', '', {
-        server_node_count: {
-            type: "number",
-            alias: 'snc',
-            default: os.cpus().length / 2,
-        },
-        port: {
-            type: "number",
+        ports: {
+            type: "array",
             alias: 'p',
-            default: 3001,
+            default: [3000],
         },
         message_interval: {
             type: "number",
             alias: 'mi',
             default: 1000
         }
-    }, handler).argv
+    }, handler)
+    .argv
 
 function handler(argv: any) {
+
     if (cluster.isPrimary) {
+        console.log(`in cluster`)
+        let workers: Worker[] = [];
+        const NODE_PORTS = argv.ports as string[];
+        for (let i = 0; i < NODE_PORTS.length; i++) {
+            let worker = cluster.fork({
+                NODE_PORT: NODE_PORTS[i],
+                MESSAGE_INTERVAL: argv.message_interval,
+            })
+            worker.on('message', (msg) => {
+                for (let j = 0; j < workers.length; j++) {
+                    if (workers[j] !== worker) {
+                        workers[j].send(msg)
+                    }
+                }
+            })
+            workers.push(worker);
+        }
+    } else {
+        console.log(`in worker`)
+        if (!process.env.NODE_PORT) {
+            throw new Error(`no port given`);
+        }
+        if(!process.env.MESSAGE_INTERVAL){
+            throw new Error(`no message interval given`)
+        }
 
+        const MESSAGE_INTERVAL = parseInt(process.env.MESSAGE_INTERVAL, 10);
+        const PORT = parseInt(process.env.NODE_PORT, 10);
+        let app = express();
+        let server = createServer(app);
+        let io = new Server(server);
+
+        server.listen(PORT, () => {
+            console.log(`server listening on port ${PORT}`)
+        })
+
+
+        io.on('connect', (socket) => {
+            socket.on('clientMessage', (msg) => {
+                // console.log(`new message from socket ${msg.socket}: ${msg.message}`)
+            });
+            socket.on("disconnect", reason => {
+                console.log(`socket ${socket.id} disconnected for reason ${reason}`)
+            })
+        })
+
+        setInterval(() => {
+            io.emit('serverMessage', {
+                message: 'server message'
+            })
+        }, MESSAGE_INTERVAL)
+
+        // setInterval(() => {
+        //     let sockets = io.sockets.sockets;
+        //     console.log(`connected clients ${Object.keys(sockets).length}`)
+        // }, 5000)
     }
-    const MESSAGE_INTERVAL = argv.message_interval as number;
-    let port = argv.port as number;
-    let app = express();
-    let server = createServer(app);
-    let io = new Server(server);
-
-    server.listen(port, () => {
-        console.log(`server listening on port ${port}`)
-    })
-
-
-    io.on('connect', (socket) => {
-        socket.on('clientMessage', (msg) => {
-            // console.log(`new message from socket ${msg.socket}: ${msg.message}`)
-        });
-        socket.on("disconnect", reason => {
-            console.log(`socket ${socket.id} disconnected for reason ${reason}`)
-        })
-    })
-
-    setInterval(() => {
-        io.emit('serverMessage', {
-            message: 'server message'
-        })
-    }, MESSAGE_INTERVAL)
-
-    setInterval(() => {
-        let sockets = io.sockets.sockets;
-        console.log(`connected clients ${Object.keys(sockets).length}`)
-    }, 5000)
 }
